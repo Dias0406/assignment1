a918ceb568c31142631b05c5ed741cc2e06f941b
Dias0406 <dias.shamel@gmail.com>
2025-12-12 13:05:24 +0500

    first commit

--- main/main.cpp
+++ main/main.cpp
@@ -6,7 +6,7 @@ int main () {
     cin >> A ;
     cin >> B;
     if (A && B <= 0) {
-        cout << "напиши другое число";
+        cout << "hello world get";
     }
     cout << A+B << endl;
     return 0;
